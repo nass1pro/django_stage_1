5cb06a67024aecfec89379cd3edc64756427d705
nass1pro <nass1pro@gmail.com>
2019-05-27 17:18:44 +0200

    .

--- quizz/questi/views.py
+++ quizz/questi/views.py
@@ -2,34 +2,36 @@
 from django.http import HttpResponse
 from .models import *
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     print("eee")
     que = prof.objects.all()
-    template = loader.get_template('index.html')
+    template = loader.get_template('questi/index.html')
     context = {'que':que}
-    return HttpResponse(template.render(context,request=request))
+    return render(request, 'questi/index.html', context)
+
 
 def detail(request, prof_id):
     print ('mois _ eee')
     ques = questionnaire.objects.filter(profs=prof_id)
 
-    template = loader.get_template('questionnaire.html')
+    template = loader.get_template('questi/questionnaire.html')
     context = {'ques':ques}
-    return HttpResponse(template.render(context,request=request))
+    return render(request, 'questi/questionnaire.html', context)
 
 def detail_questionnaire(request, prof_id):
     print ('mo')
     ques = questionnaire.objects.get(profs=prof_id)
-    template = loader.get_template('questionnaires.html')
+    template = loader.get_template('questi/questionnaires.html')
     context = {'ques':ques}
-    return HttpResponse(template.render(context,request=request))
+    return render(request, 'questi/questionnaires.html', context)
 
 def detail_questions(request, questionnaire_id):
     print ('moissss')
     quess = questions.objects.get(questionnaires=questionnaire_id)
-    template = loader.get_template('questions.html')
+    template = loader.get_template('questi/questions.html')
     context = {'quess':quess}
-    return HttpResponse(template.render(context,request=request))
+    return render(request, 'questi/questions.html', context)
